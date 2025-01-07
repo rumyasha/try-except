6eb626c11190bb5f18c9b683a06d3637f546c453
rumyasha <azimkulovarita019@gmail.com>
2025-01-08 01:13:48 +0600

    Create my_lists.py

---
+++ my_lists.py
@@ -0,0 +1,23 @@
+def sort_list(x, reverse=False):
+    return sorted(x, reverse=reverse)
+
+
+def get_max(x):
+    return max(x)
+
+
+def get_min(x):
+    return min(x)
+
+
+def clear_list(x):
+    x.clear()
+    return x
+
+
+def sum_of_elements(x):
+    return sum(x)
+
+
+def count_elements(x):
+    return len(x)
