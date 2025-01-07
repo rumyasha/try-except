8928a88a7de0842ac133f7d4caaa1050e07e42f6
rumyasha <azimkulovarita019@gmail.com>
2025-01-08 01:13:25 +0600

    Create my_math.py

---
+++ my_math.py
@@ -0,0 +1,22 @@
+def add(a, b):
+    return a + b
+
+
+def subtract(a, b):
+    return a - b
+
+
+def multiply(a, b):
+    return a * b
+
+
+def power(a, b):
+    return a ** b
+
+
+def moduls(a, b):
+    return a % b
+
+
+def floor_division(a, b):
+    return a // b
